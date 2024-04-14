b054a22d40605e4ec608a313fd892ce09001957c
José Eduardo Neves <joseeduardomioto@gmail.com>
2024-04-14 10:48:27 -0300

    Aula 1 do curso de C

---
+++ Exercicios/aula-1/aula1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int arg, char *argv[]){
+	
+	printf("msg1! \n");
+	printf("msg2! \n");
+	printf("msg3! \n");
+	printf("msg4! \n");
+	printf("msg5! \n");
+	return 0;
+}
